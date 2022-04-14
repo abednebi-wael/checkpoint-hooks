de2ca0627a54c7a38f97018223158a1961e1480b
core.pager=cat <waelabednebi3@gmail.com>
2022-04-15 11:10:59 +1400

    new file

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+function HomePage () {
+    return (
+      <div className ="home_page">
+        <h1> Watch In</h1>
+        <br/>
+        <h2>Welcome to our platform </h2>
+
+      </div>
+    );
+}
+export default HomePage;
